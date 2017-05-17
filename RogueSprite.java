07b3f4b259f7eaf9ad12febc709e3beda509122d
JonCattuna <jcattuna@gmail.com>
2017-05-17 08:49:23 -0400

    Unformatted Sprite 5/17

--- RogueSprite.java
+++ RogueSprite.java
@@ -18,57 +18,57 @@ public class RogueSprite
 
  	int x, y, w, h;
 
- public RogueSprite(int x,int y ,int w, int h)
- {
-	this.x = x;
-	this.y = y;
-	this.h = h;
-	this.w = w;
+	public RogueSprite(int x,int y ,int w, int h)
+	{
+		this.x = x;
+			this.y = y;
+		this.h = h;
+		this.w = w;
 
-	Toolkit tk = Toolkit.getDefaultToolkit();
+		Toolkit tk = Toolkit.getDefaultToolkit();
 
-	Back = tk.getImage("back5.png");
-	LBrick = tk.getImage("LongWall.png");
-    Brick = tk.getImage("brick.png");
+		Back = tk.getImage("back5.png");
+		LBrick = tk.getImage("LongWall.png");
+	    Brick = tk.getImage("brick.png");
 
-    r1 = new Rectangle(x,y,w,h);
-  }
+	    r1 = new Rectangle(x,y,w,h);
+	}
 
-  public Rectangle getRectangle()
-  {
-	  return r1;
-  }
-
-  public boolean collide(Rectangle r2)
-  {
-	  if (r1.intersects(r2))
+	  public Rectangle getRectangle()
 	  {
-		  return true;
-
+		  return r1;
 	  }
-	  else
-	  return false;
-  }
 
-  public void setPosition(int x, int y)
-  {
-	  this.x = x;
-	  this.y = y;
-  }
-  public int getPositionX()
-  {
-	  return x;
-  }
-  public int getPositionY()
-  {
-	  return y;
-  }
-  public void draw (Graphics g)
-  {
-	  g.drawImage(Brick,x,y,null);
-	  g.setColor(Color.BLUE);
-	  r1 = new Rectangle(x,y,w,h);
-	  ((Graphics2D)g).draw(r1);
-   }
+	  public boolean collide(Rectangle r2)
+		{
+			if (r1.intersects(r2))
+		  {
+			  return true;
+
+		  }
+		  else
+		  return false;
+	  }
+
+	  public void setPosition(int x, int y)
+	  {
+		  this.x = x;
+		  this.y = y;
+	  }
+	  public int getPositionX()
+	  {
+		  return x;
+	  }
+	  public int getPositionY()
+	  {
+		  return y;
+	  }
+	  public void draw (Graphics g)
+	  {
+		  g.drawImage(Brick,x,y,null);
+		  g.setColor(Color.BLUE);
+		  r1 = new Rectangle(x,y,w,h);
+		  ((Graphics2D)g).draw(r1);
+	   }
 
 }
