44b2c4bc282cb0d2c7144fabe0e9669236fab380
JonCattuna <jcattuna@gmail.com>
2017-05-17 08:47:09 -0400

    5/17 Rogue Sprite and Driver

--- RogueDriver.java
+++ RogueDriver.java
@@ -4,20 +4,33 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.applet.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 
 public class RogueDriver extends JApplet
 {
+	Random r;
 	RogueSprite rog;
+	ArrayList<RogueSprite> Bricks;
+	int x,y ,w,h;
+
 
  	public void init()
 	{
-		 rog = new RogueSprite();
-  	setContentPane(new DrawingPanel());
-  	setLayout(null);
+		r = new Random();
+		Bricks = new ArrayList<RogueSprite>();
+		setContentPane(new DrawingPanel());
+		for(int k = 0; k <50; k++)
+		{
+			x = r.nextInt(1000);
+			y = r.nextInt(1000);
+			w = 100;
+			h = 26;
+			Bricks.add(new RogueSprite(x,y,w,h));
+		}
 
-
+			setLayout(null);
   }
 
    public class DrawingPanel extends JPanel
@@ -25,9 +38,11 @@ public class RogueDriver extends JApplet
        public void paintComponent(Graphics g)
        {
    		 super.paintComponent(g);
-    	rog.draw(g);
 
-
+   		 for(int k = 0; k <50; k++)
+		 {
+			 Bricks.get(k).draw(g);
+		 }
        }
    }
 }
