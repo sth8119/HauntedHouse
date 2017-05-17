87cee826608fb96831dc652448faa49220b65fe4
sth8119 <sth8119@gmail.com>
2017-05-17 09:12:58 -0400

    Update RogueSprite formatting

--- RogueSprite.java
+++ RogueSprite.java
@@ -3,72 +3,60 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.applet.*;
 import java.util.Random;
-
-
 public class RogueSprite
 {
- 	Image Brick;
- 	Image LBrick;
- 	Image Back;
-
- 	Rectangle r1;
-
- 	double d1;
- 	double d2;
-
- 	int x, y, w, h;
-
-	public RogueSprite(int x,int y ,int w, int h)
+	Image brick;
+	Image lBrick;
+	Image back;
+	Rectangle rectangle;
+	double d1;
+	double d2;
+	int x, y, width, height;
+	public RogueSprite(int x,int y ,int width, int height)
 	{
 		this.x = x;
-			this.y = y;
-		this.h = h;
-		this.w = w;
-
+		this.y = y;
+		this.width = width;
+		this.height = height;
 		Toolkit tk = Toolkit.getDefaultToolkit();
-
-		Back = tk.getImage("back5.png");
-		LBrick = tk.getImage("LongWall.png");
-	    Brick = tk.getImage("brick.png");
-
-	    r1 = new Rectangle(x,y,w,h);
+		back = tk.getImage("back5.png");
+		lBrick = tk.getImage("LongWall.png");
+		brick = tk.getImage("brick.png");
+		rectangle = new Rectangle(x,y,width,height);
 	}
-
-	  public Rectangle getRectangle()
-	  {
-		  return r1;
-	  }
-
-	  public boolean collide(Rectangle r2)
+	public Rectangle getRectangle()
+	{
+		return rectangle;
+	}
+	public boolean collide(Rectangle r)
+	{
+		if (rectangle.intersects(r))
 		{
-			if (r1.intersects(r2))
-		  {
-			  return true;
-
-		  }
-		  else
-		  return false;
-	  }
-
-	  public void setPosition(int x, int y)
-	  {
-		  this.x = x;
-		  this.y = y;
-	  }
-	  public int getPositionX()
-	  {
-		  return x;
-	  }
-	  public int getPositionY()
-	  {
-		  return y;
-	  }
-	  public void draw (Graphics g)
-	  {
-		  g.drawImage(Brick,x,y,null);
-		  g.setColor(Color.BLUE);
-		  r1 = new Rectangle(x,y,w,h);
-		  ((Graphics2D)g).draw(r1);
-	   }
-
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public void setPosition(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	public int getPositionX()
+	{
+		return x;
+	}
+	public int getPositionY()
+	{
+		return y;
+	}
+	public void draw (Graphics g)
+	{
+		g.drawImage(brick,x,y,null);
+		g.setColor(Color.BLUE);
+		//rectangle = new Rectangle(x,y,w,h);
+		((Graphics2D)g).draw(rectangle);
+	}
 }
