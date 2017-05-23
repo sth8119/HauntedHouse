e9a343d48ce08cb3bf1629a7813ec4d0d28aed34
sth8119 <sth8119@gmail.com>
2017-05-23 09:00:58 -0400

    Updated Sprites

--- RogueDriver.java
+++ RogueDriver.java
@@ -1,3 +1,5 @@
+
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -13,26 +15,49 @@ public class RogueDriver extends JApplet
 	//IMPORTANT- Make sure Quinn uses the current Sprite class- it will be in the main branch of the Github repository
 	Sprite sprite; //16 Sprites- Name them similar to the file names I made to make it easier to keep track
 	BufferedImage brickImage; //16 BufferedImages
-	//Sprite[] animations = new Sprite[16]; //This array list will
+	ArrayList<BufferedImage> animations; //This array list will
 	public void init()
 	{
+		animations  = new ArrayList<BufferedImage>();
 		setLayout(null);
 		setContentPane(new DrawingPanel());
 		try
 		{
-			brickImage = ImageIO.read(new File("brick.png")); //Do this once for each character model - There should be 16 total
+			brickImage = ImageIO.read(new File("brick.png"));
+			animations.add(ImageIO.read(new File("Player1_Down.png")));
+			animations.add(ImageIO.read(new File("Player1_DownRun.png")));
+			animations.add(ImageIO.read(new File("Player1_Down2.png")));
+			animations.add(ImageIO.read(new File("Player1_DownRun2.png")));
+			animations.add(ImageIO.read(new File("Player1_Up.png")));
+			animations.add(ImageIO.read(new File("Player1_UpRun.png")));
+			animations.add(ImageIO.read(new File("Player1_Up2.png")));
+			animations.add(ImageIO.read(new File("Player1_UpRun2.png")));
+			animations.add(ImageIO.read(new File("Player1_Left.png")));
+			animations.add(ImageIO.read(new File("Player1_LeftRun.png")));
+			animations.add(ImageIO.read(new File("Player1_Left2.png")));
+			animations.add(ImageIO.read(new File("Player1_LeftRun2.png")));
+			animations.add(ImageIO.read(new File("Player1_Right.png")));
+			animations.add(ImageIO.read(new File("Player1_RightRun.png")));
+			animations.add(ImageIO.read(new File("Player1_Right2.png")));
+			animations.add(ImageIO.read(new File("Player1_RightRun2.png")));
+
+			sprite = new Sprite(animations, 100, 100, 100,100);
+
+			//Do this once for each character model - There should be 16 total
 		}
 		catch(Exception e)
 		{
 			System.out.println("File Not Found");
 		}
-		sprite = new Sprite(brickImage, 50, 50, brickImage.getWidth(), brickImage.getHeight()); //50, 50 is just the initial x and y for the image
+		//sprite = new Sprite(brickImage, 50, 50, brickImage.getWidth(), brickImage.getHeight()); //50, 50 is just the initial x and y for the image
 	}
 	public class DrawingPanel extends JPanel
 	{
 		public void paintComponent(Graphics g)
 		{
 			super.paintComponent(g);
+			//animations.get(0).draw(g);
+
 			sprite.draw(g); //Use a timer to change the image ever few milliseconds for now. Once the image index reaches 16, it should go back to index = 0
 		}
 	}
